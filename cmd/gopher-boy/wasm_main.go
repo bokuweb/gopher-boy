03303115954a3da5b794760e976c9d747653a436
bokuweb <bokuweb12@gmail.com>
2019-10-12 16:02:01 +0900

    fix: tile data index

--- cmd/gopher-boy/wasm_main.go
+++ cmd/gopher-boy/wasm_main.go
@@ -12,6 +12,7 @@ import (
 	"github.com/bokuweb/gopher-boy/pkg/interrupt"
 	"github.com/bokuweb/gopher-boy/pkg/logger"
 	"github.com/bokuweb/gopher-boy/pkg/pad"
+	"github.com/bokuweb/gopher-boy/pkg/types"
 	"github.com/bokuweb/gopher-boy/pkg/window"
 
 	"github.com/bokuweb/gopher-boy/pkg/gpu"
@@ -65,6 +66,9 @@ func newGB(this js.Value, args []js.Value) interface{} {
 	this.Set("getVRAM", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
 		d := vRAM.GetBuf()
 		return js.TypedArrayOf(d)
+	}))
+	this.Set("readGPU", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		return gpu.Read(types.Word(args[0].Int()))
 	}))
 	return this
 }
